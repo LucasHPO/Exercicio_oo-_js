6cef1fb403384584f9966c2589750dd50b4ddbfa
Lucas Hiago <lucashiago620@hotmail.com>
2023-05-22 14:54:37 -0300

    FIX: Correção no ano de lançamento

--- main.js
+++ main.js
@@ -8,24 +8,24 @@ class Titulo {
 }
 
 class Filme extends Titulo{
-    constructor (nome, genero,classificacao, duracao, diretor) {
-        super(nome, genero,classificacao);
+    constructor (nome, genero, classificacao, anoDeLancamento, duracao, diretor) {
+        super(nome, genero, classificacao, anoDeLancamento);
         this.duracao = duracao;
         this.diretor = diretor;
     }
 }
 
 class Serie extends Titulo{
-    constructor (nome, genero,classificacao, temporadas, numeroDeEpisodios){
-        super(nome, genero,classificacao)
+    constructor (nome, genero, classificacao, anoDeLancamento, temporadas, numeroDeEpisodios){
+        super(nome, genero, classificacao, anoDeLancamento)
         this.temporadas = temporadas;
         this.numeroDeEpisodios = numeroDeEpisodios;
     }
 }
 
-const detonaRalph = new Filme ("Detona Ralph", "Animação", 2012, "Livre",101, "Rich Moore");
-const fordVSFerrari = new Filme ("Ford VS Ferrari", "Drama/Esporte", 2019, "+12",152, "Rich Moore");
-const driveToSurvive = new Serie ("Drive To Survive", "Documentários",2019 , "+12", 5, 50);
+const detonaRalph = new Filme ("Detona Ralph", "Animação", "Livre", 2012,101, "Rich Moore");
+const fordVSFerrari = new Filme ("Ford VS Ferrari", "Drama/Esporte", "+12", 2019,152, "Rich Moore");
+const driveToSurvive = new Serie ("Drive To Survive", "Documentários", "+12",2019 , 5, 50);
 
 console.log(detonaRalph);
 console.log(fordVSFerrari);
